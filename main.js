fdad79a9ad4ae0756b4a292e4d522fb5e36182a7
Abid Hasan Jaber <abidhasanjaber2320@gmail.com>
2024-04-26 18:46:56 +0600

    second commit fixed some little bug

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let input = document.querySelector(".input-field");
 let addBtn = document.querySelector(".add-btn");
 let todos = document.querySelector(".todos");
+let defaultText = document.querySelector(".default-text")
 
 let todosList = [];
 
@@ -10,7 +11,7 @@ if(localStorage.getItem("todosList")){
     render();
 } else {
     // Add default todo item only if local storage is empty
-    todosList.push({ todo: "Add Todo", id: 1 });
+    todosList.push({ todo: "Add Todo", id: 1, complete: false});
     saveToLocalStorage(); // Save the default todo to local storage
     render();
 }
@@ -20,7 +21,7 @@ addBtn.addEventListener("click", handleTodos);
 function handleTodos() {
     if (input.value) {
         let value = input.value;
-        let newTodo = { todo: value, id: todosList.length + 1 };
+        let newTodo = { todo: value, id: todosList.length + 1, complete: false };
         todosList.push(newTodo);
         saveToLocalStorage();
         render();
@@ -34,31 +35,51 @@ function handleDelete(id) {
     render();
 }
 
+function handleComplete(id) {
+    // Find the index of the item with the given ID
+    const index = todosList.findIndex(item => item.id === id);
+    if (index !== -1) {
+        // Update the 'complete' property of the found item
+        todosList[index].complete = !todosList[index].complete;
+        saveToLocalStorage();
+        render();
+    }
+}
+
 function saveToLocalStorage(){
     localStorage.setItem("todosList", JSON.stringify(todosList));
 }
 
 function render() {
     todos.innerHTML = "";
-    todosList.forEach((item) => {
-        let todoText = document.createElement("p");
-        let deleteBtn = document.createElement("button");
-        let completeBtn = document.createElement("button");
-        deleteBtn.classList.add("delete");
-        completeBtn.classList.add("complete");
-        todoText.classList.add("todo");
-        let div = document.createElement("div");
-        div.classList.add("flex");
-        div.appendChild(completeBtn);
-        completeBtn.innerHTML = "&#10003;";
-        completeBtn.setAttribute("id", item.id);
-        todoText.textContent = item.todo;
-        todoText.setAttribute("id", item.id);
-        div.appendChild(todoText);
-        div.appendChild(deleteBtn);
-        deleteBtn.innerHTML = "&#128465;";
-        deleteBtn.setAttribute("id", item.id);
-        todos.appendChild(div);
-        deleteBtn.addEventListener("click", () => handleDelete(item.id));
-    });
+    if(todosList){
+        todosList.forEach((item) => {
+            console.log(item);
+            let todoText = document.createElement("p");
+            let deleteBtn = document.createElement("button");
+            let completeBtn = document.createElement("button");
+            deleteBtn.classList.add("delete");
+            completeBtn.classList.add("complete");
+            todoText.classList.add("todo");
+            let div = document.createElement("div");
+            div.classList.add("flex");
+            div.appendChild(completeBtn);
+            completeBtn.innerHTML = "&#10003;";
+            completeBtn.setAttribute("id", item.id);
+            todoText.textContent = item.todo;
+            todoText.setAttribute("id", item.id);
+            div.appendChild(todoText);
+            div.appendChild(deleteBtn);
+            deleteBtn.innerHTML = "&#128465;";
+            deleteBtn.setAttribute("id", item.id);
+            if(item.complete){
+                todoText.classList.add("checked")
+                completeBtn.style.backgroundColor = "#FA7070";
+                completeBtn.style.color = "white";
+            }
+            deleteBtn.addEventListener("click", () => handleDelete(item.id));
+            completeBtn.addEventListener("click", ()=> handleComplete(item.id))
+            todos.appendChild(div);
+        });
+    }
 }
